c2d044061f5da774265b71db24d36c9e6fcc274c
Lukas Wöhrl <lukas.woehrl@plentymarkets.com>
2023-02-22 19:28:46 +0100

    add generic

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module woehrl01/aws_auth_jwt
 go 1.19
 
 require (
+	github.com/go-openapi/runtime v0.19.24
 	github.com/golang-jwt/jwt/v5 v5.0.0-rc.1
 	github.com/hashicorp/vault v1.12.3
 	github.com/hashicorp/vault/api v1.9.0
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/go-openapi/runtime/middleware/header"
 	"github.com/golang-jwt/jwt/v5"
 	vault "github.com/hashicorp/vault/api"
 	auth "github.com/hashicorp/vault/api/auth/aws"
@@ -17,24 +18,20 @@ import (
 	"github.com/hashicorp/vault/sdk/logical"
 )
 
-func startServer() {
-	http.HandleFunc("/v1/auth/aws/login", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Received request: %s\n", r.URL.Path)
+func setupConfig() *logical.InmemStorage {
+	context := context.Background()
+	storage := &logical.InmemStorage{}
+	storage.Put(context, &logical.StorageEntry{Key: "config/client", Value: []byte("{}")});
+	// Add generic role, this is needed for the login to work
+	storage.Put(context, &logical.StorageEntry{Key: "role/generic", Value: []byte(`{"auth_type": "iam","version":3}`)});
 
-		var data map[string]interface{}
-		err := json.NewDecoder(r.Body).Decode(&data)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		storage := &logical.InmemStorage{}
-		storage.Put(r.Context(), &logical.StorageEntry{Key: "config/client", Value: []byte("{}")});
-
-		//read allowed roles from file iterate over every line and add it to the storage
+	//read allowed roles from file iterate over every line and add it to the storage
+	//check if file exists
+	if _, err := os.Stat("allowed_roles.txt"); os.IsNotExist(err) {
 		file, err := os.Open("allowed_roles.txt")
 		if err != nil {
 			fmt.Println("Error opening file:", err)
-			return
+			return nil
 		}
 		defer file.Close()
 
@@ -44,7 +41,7 @@ func startServer() {
 		// Process each line
 		for scanner.Scan() {
 			role := scanner.Text()
-			storage.Put(r.Context(), &logical.StorageEntry{Key: "role/"+strings.ToLower(role), Value: []byte(`{"auth_type": "iam","version":3}`)});
+			storage.Put(context, &logical.StorageEntry{Key: "role/"+strings.ToLower(role), Value: []byte(`{"auth_type": "iam","version":3}`)});
 			fmt.Printf("Added role: %s\n", role)
 		}
 
@@ -52,9 +49,47 @@ func startServer() {
 		if err := scanner.Err(); err != nil {
 			fmt.Println("Error reading file:", err)
 		}
+	}
+	return storage
+}
+
+func startServer() {
+
+	storage := setupConfig()
+	
+	http.HandleFunc("/v1/auth/aws/login", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Received request: %s\n", r.URL.Path)
+
+		// Check if the request is a PUT
+		if r.Method != "PUT" {
+			msg := "Request method is not PUT"
+			http.Error(w, msg, http.StatusMethodNotAllowed)
+			return
+		}
+
+		// Check if the Content-Type is application/json
+		if r.Header.Get("Content-Type") != "" {
+			value, _ := header.ParseValueAndParams(r.Header, "Content-Type")
+			if value != "application/json" {
+				msg := "Content-Type header is not application/json"
+				http.Error(w, msg, http.StatusUnsupportedMediaType)
+				return
+			}
+		}
+
+		// Limit the request body to 1MB
+		r.Body = http.MaxBytesReader(w, r.Body, 1048576)
+
+		// Decode the request body into a map
+		var data map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			msg := "Request body could not be decoded into JSON"
+			http.Error(w, msg, http.StatusBadRequest)
+			return
+		}
 
 		backend,_ := awsauth.Backend(&logical.BackendConfig{})
-
 		response, _ := backend.HandleRequest(r.Context(), &logical.Request{
 			Storage:   storage,
 			Operation: logical.UpdateOperation,
@@ -62,12 +97,16 @@ func startServer() {
 			Data: data,
 		})
 
-		if !response.IsError() {
-
+		if response.IsError() {
+			w.WriteHeader(http.StatusUnauthorized)
+			fmt.Println("Login failed")
+		} else {
 			fmt.Println("Login successful")
 
 			token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-				"foo": "bar",
+				"canonical_arn": response.Auth.InternalData["canonical_arn"],
+				"account_id": response.Auth.InternalData["account_id"],
+				"display_name": response.Auth.DisplayName,
 				"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
 			})
 
@@ -78,7 +117,7 @@ func startServer() {
 					ClientToken: tokenString,
 				},
 				Data: map[string]interface{}{
-					"cannonical_arn": response.Auth.InternalData["cannonical_arn"],
+					"canonical_arn": response.Auth.InternalData["canonical_arn"],
 					"account_id": response.Auth.InternalData["account_id"],
 					"display_name": response.Auth.DisplayName,
 				},
@@ -86,9 +125,7 @@ func startServer() {
 
 			w.WriteHeader(http.StatusOK)
 			json.NewEncoder(w).Encode(secret)
-		} else {
-			w.WriteHeader(http.StatusUnauthorized)
-		}
+		} 
 	})
 
 	go http.ListenAndServe(":8081", nil)
@@ -100,25 +137,24 @@ func doLogin(){
 	config.Address = "http://localhost:8081";
 
     client, _ := vault.NewClient(config)
-	awsAuth, _ := auth.NewAWSAuth()
+	awsAuth, _ := auth.NewAWSAuth(
+		auth.WithRole("generic"), // we use a generic role, because we don't have a role in the storage
+	)
 	authInfo, err := awsAuth.Login(context.Background(), client)
-
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	b, err := json.Marshal(authInfo)
+	b, err := json.MarshalIndent(authInfo, "", "  ")
 	if err != nil {
 		fmt.Println(err)
 	}
-
 
 	fmt.Println()
 	fmt.Printf("AuthInfo: %s\n", b)
 }
 
 func main() {
-
 	startServer()
 
 	doLogin()
